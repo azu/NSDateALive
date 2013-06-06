1bed5cce2dd392c83d48e07296d2522e22052b7b
azu <azuciao@gmail.com>
2013-06-06 16:59:56 +0900

    Initial Commit

---
+++ NSDateALive/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSDateALive
+//
+//  Created by azu on 2013/06/06.
+//  Copyright (c) 2013年 azu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NSDateALiveTests/NSDateALiveTests.h
@@ -0,0 +1,13 @@
+//
+//  NSDateALiveTests.h
+//  NSDateALiveTests
+//
+//  Created by azu on 2013/06/06.
+//  Copyright (c) 2013年 azu. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NSDateALiveTests : SenTestCase
+
+@end
